96988746b3e8e77cde5c0ee2fffe3cc0bd75121d
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-11-22 16:13:45 +0000

    Added colourTexture to shader

--- shaders/fragment.hlsl
+++ shaders/fragment.hlsl
@@ -10,9 +10,12 @@ struct PSOutput
     float4 colour : SV_Target0;
 };
 
+Texture2D cTex : register(t0);
+SamplerState sampler0 : register(s0);
+
 
 PSOutput Main(PSInput input)
 {
-    PSOutput output = { input.normal / 2 + 0.5, 1 };
+    PSOutput output = { cTex.SampleLevel(sampler0,input.uv,0).xyz, 1 };
     return output;
 }
