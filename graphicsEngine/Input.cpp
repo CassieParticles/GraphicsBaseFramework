749d23975dedc67f59add44371b727ba5927209c
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-11-24 00:41:38 +0000

    Added ImGui to graphics base framework, moved GLFW headers

--- graphicsEngine/Input.cpp
+++ graphicsEngine/Input.cpp
@@ -4,7 +4,7 @@
 #include <graphicsEngine/Window.h>
 
 #include <iostream>
-#include <glfw3.h>
+#include <GLFW/glfw3.h>
 
 bool InputHandler::instantiated = false;
 
--- graphicsEngine/Window.cpp
+++ graphicsEngine/Window.cpp
@@ -1,11 +1,11 @@
 #include "Window.h"
 #include "Window.h"
 
-#include <glfw3.h>
+#include <GLFW/glfw3.h>
 #include <d3dcompiler.h>
 
 #define GLFW_EXPOSE_NATIVE_WIN32
-#include <glfw3native.h>
+#include <GLFW/glfw3native.h>
 
 #include <iostream>
 
